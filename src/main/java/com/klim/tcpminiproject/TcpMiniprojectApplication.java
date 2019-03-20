d258dd648ef8fc05b33384538dbba3bf4960b1e8
miketheklim <miketheklim@icloud.com>
2019-03-20 16:13:07 +0100

    spring boot app init

---
+++ src/main/java/com/klim/tcpminiproject/TcpMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package com.klim.tcpminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpMiniprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcpMiniprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klim/tcpminiproject/TcpMiniprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.klim.tcpminiproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TcpMiniprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
